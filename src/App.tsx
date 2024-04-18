939595f22a25a28b0130e4010de0b7a9524b4ac9
ipi19 <ipyanamwakikunga@gmail.com>
2024-04-19 01:36:01 +0200

    menuBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./componets/NavBar";
-import { Route, Routes } from "react-router-dom";
-import Home from "./componets/pages/Home";
-import DrinkingWater from "./componets/pages/DrinkingWater";
-import Excersises from "./componets/pages/Excersises";
-import Phone from "./componets/pages/Phone";
-import Tasks from "./componets/pages/Tasks";
-import Food from "./componets/pages/Food";
+import NavContent from "./componets/NavContent";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Grid
         templateAreas={{
           base: `"nav" "main"`,
           lg: `"nav nav " "aside main "`,
         }}
       >
-        <GridItem area={"nav"} bgColor={"blue"}>
+        <GridItem area={"nav"}>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-
-            <Route path="/drinking" element={<DrinkingWater />} />
-            <Route path="/excersises" element={<Excersises />} />
-            <Route path="/phone" element={<Phone />} />
-            <Route path="/tasks" element={<Tasks />} />
-            <Route path="/food" element={<Food />} />
-          </Routes>
+          <NavContent />
         </GridItem>
         <Show above="lg">
           <GridItem area={"aside"} bgColor={"yellow"}>
@@ -39,7 +25,7 @@ const App = () => {
           main
         </GridItem>
       </Grid>
-    </div>
+    </>
   );
 };
 
---
+++ src/componets/NavContent.tsx
@@ -0,0 +1,76 @@
+import { Link, Routes, Route } from "react-router-dom";
+import DrinkingWater from "./pages/DrinkingWater";
+import Food from "./pages/Food";
+import Home from "./pages/Home";
+import Phone from "./pages/Phone";
+import Tasks from "./pages/Tasks";
+import {
+  HStack,
+  Hide,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Show,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+
+const NavContent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <HStack justifyContent={"right"}>
+        {" "}
+        {/* Show menu button on small screens */}
+        <Menu
+          isOpen={isOpen}
+          onOpen={handleToggleMenu}
+          onClose={handleToggleMenu}
+        >
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<HamburgerIcon />}
+            variant="outline"
+            onClick={handleToggleMenu}
+          />
+
+          <MenuList>
+            <MenuItem as={Link} to="/">
+              Home
+            </MenuItem>
+            <MenuItem as={Link} to="/drinking">
+              DrinkingWater
+            </MenuItem>
+            <MenuItem as={Link} to="/phone">
+              Phone
+            </MenuItem>
+            <MenuItem as={Link} to="/tasks">
+              Tasks
+            </MenuItem>
+            <MenuItem as={Link} to="/food">
+              Food
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </HStack>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/drinking" element={<DrinkingWater />} />
+
+        <Route path="/phone" element={<Phone />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/food" element={<Food />} />
+      </Routes>
+    </>
+  );
+};
+
+export default NavContent;
